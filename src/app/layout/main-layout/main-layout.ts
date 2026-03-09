90faf8ddc1fcd7a016d362b70acffe596a71564c
Abdukholiq G'ijdvoniy <abdukholiq23@gmail.com>
2026-03-09 19:47:35 +0500

    feat(menu-bar): create menu bar

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBar } from "../../shared/components/menu-bar/menu-bar";
 
 @Component({
   selector: 'app-main-layout',
   imports: [
-    RouterOutlet
-  ],
+    RouterOutlet,
+    MenuBar
+],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
--- src/app/shared/components/menu-bar/menu-bar.ts
+++ src/app/shared/components/menu-bar/menu-bar.ts
@@ -1,21 +1,53 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-bar.html',
   styleUrl: './menu-bar.scss',
 })
-export class MenuBar {
-  protected items = signal<AppMenuItems[]>([
+export class MenuBar implements OnInit {
+  private readonly router = inject(Router);
+
+  protected activeItem = signal<string>('');
+
+  protected items = signal<MenuBarItem[]>([
     {
-      name: 'Queue',
+      label: 'Home',
       path: '',
+      icon: 'pi pi-home',
+    },
+    {
+      label: 'Queues',
+      path: '',
+      icon: 'pi pi-list',
+    },
+    {
+      label: 'Profile',
+      path: '',
+      icon: 'pi pi-user',
     },
   ]);
+
+  ngOnInit() {
+    const currentPath = window.location.pathname;
+    console.log(currentPath);
+    
+
+    switch (currentPath) {
+      case '/':
+        this.activeItem.set('Home');
+        break;
+      case '/queues':
+        this.activeItem.set('Queues');
+        break;
+    }
+  }
 }
 
-export interface AppMenuItems {
-  name: string;
-  path: string;
+export interface MenuBarItem {
+  label?: string;
+  path?: string;
+  icon?: string;
 }
