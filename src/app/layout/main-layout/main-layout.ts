57586e051ce8d979648c3d59352d1cad87d3cc32
Abdukholiq NG <abdukholiq23@gmail.com>
2026-03-11 10:32:42 +0500

    fix(main-layout): define layout

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuBar } from "../../shared/components/menu-bar/menu-bar";
+import { Navbar } from "../../shared/components/navbar/navbar";
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     RouterOutlet,
-    MenuBar
-],
+    MenuBar,
+    Navbar
+  ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
-export class MainLayout {}
+export class MainLayout { }
