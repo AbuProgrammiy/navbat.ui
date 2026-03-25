8bca92bbea68def4d567b8d75f0e81d9ac4398ca
Abdukholiq NG <abdukholiq23@gmail.com>
2026-03-25 18:39:15 +0500

    add animation

--- src/app/feature/home/home.ts
+++ src/app/feature/home/home.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { FilterBar } from "./components/filter-bar/filter-bar";
 
 @Component({
   selector: 'app-home',
-  imports: [FilterBar],
+  imports: [FilterBar, NgClass],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home implements OnInit {
-  test: any = null;
-  ngOnInit(): void {
-    this.test = 'hech nima yoq';
-  }
+export class Home {
+  protected isFilterCollapsed = signal<boolean>(false);
 }
