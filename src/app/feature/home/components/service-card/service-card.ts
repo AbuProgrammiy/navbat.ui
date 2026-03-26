bf08d0779df0165495ac3b68c875735d8948b5e4
Abdukholiq NG <abdukholiq23@gmail.com>
2026-03-26 19:07:34 +0500

    latest

---
+++ src/app/feature/home/components/service-card/service-card.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Image } from 'primeng/image';
+
+@Component({
+  selector: 'app-service-card',
+  imports: [
+    Image
+  ],
+  templateUrl: './service-card.html',
+  styleUrl: './service-card.scss',
+})
+export class ServiceCard {
+
+}
--- src/app/feature/home/home.ts
+++ src/app/feature/home/home.ts
@@ -1,10 +1,11 @@
 import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FilterBar } from "./components/filter-bar/filter-bar";
+import { ServiceCard } from "./components/service-card/service-card";
 
 @Component({
   selector: 'app-home',
-  imports: [FilterBar, NgClass],
+  imports: [FilterBar, NgClass, ServiceCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
